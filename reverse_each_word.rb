d4e1b490faace43889627ca1c5df35d95c7ed318
Charlie Oster <oster.charlie@yahoo.com>
2019-10-11 23:49:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
-names ['Charlie','Juan','John']
-
-names.each do
-  puts "Hello there, and how are you?, #{names}"
-  puts name.upcase.reverse
+puts "Hello there, and how are you?"
+puts upcase.reverse
 end
